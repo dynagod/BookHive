28a891fbb3fccedef00b98e98aeb60cc2099479e
Asmit Gupta <guptaasmit12345@gmail.com>
2025-04-16 15:09:22 +0530

    Created some frontend components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,38 @@
-const App = () => {
+
+import { Outlet } from 'react-router-dom'
+
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import { useEffect, useState } from 'react'
+import Loading from './components/Loading'
+
+function App() {
+
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 2000); 
+
+    // Cleanup timer
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (loading) {
+    return <Loading />; 
+  }
+
 
   return (
     <>
-      <div className='bg-zinc-900 text-white min-h-screen'>
-        BookHive
-      </div>
+            <Navbar />
+        <main className='min-h-screen max-w-screen-2xl mx-auto px-4 py-6 font-primary'>
+          <Outlet />
+        </main>
+        <Footer />
+      
     </>
   )
 }
--- frontend/src/components/Loading.jsx
+++ frontend/src/components/Loading.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from 'react';
 
 const Loading = () => {
   return (
-    <div>Loading</div>
-  )
-}
+    <div className="flex justify-center items-center h-screen">
+      <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-blue-500 border-solid"></div>
+    </div>
+  );
+};
 
-export default Loading
+export default Loading;
